7e912e1257ba9116657ce21fa595e59a382794ea
06megha12 <112416048+06megha12@users.noreply.github.com>
2022-09-20 00:48:47 +0530

    Add files via upload

---
+++ Spring-boot-crud-application-jpa-h2/src/main/java/com/springboot/SpringBootCrudApplicationJpaH2Application.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudApplicationJpaH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudApplicationJpaH2Application.class, args);
+	}
+
+}
